2b42e68902241cac9400e7ce875c2ad5724b1768
sokalo.vo <sokalo.vo@HM-0934-DEVELOP>
2016-10-26 12:14:44 +0300

    message model done

--- Chasok4/Models/Entities/Message.cs
+++ Chasok4/Models/Entities/Message.cs
@@ -6,10 +6,21 @@ using System.Web;
 namespace Chasok4.Models.Entities
 {
     public class Message
-    {       
+    {
+        public Message()
+        {
+            AllUsersMessageTo = new List<AppUser>();
+            ReadStatus = false;
+        }
         public int Id { get; set; }
         public string Body { get; set; }
-        public string RoomName { get; set; }
-        public List<string> Friends { get; set; }
+        public DateTime CreateData { get; set; }
+        public DateTime? ReadData { get; set; }
+
+        public bool ReadStatus { get; set; }
+        public virtual List<AppUser> AllUsersMessageTo { get; set; }
+
+        public virtual AppUser Creator { get; set; }
+
     }
 }
