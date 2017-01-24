1c06002ab500638ea04125f73d622f20dc3ed83d
sokalo.vo <sokalo.vo@HM-0934-DEVELOP>
2017-01-24 15:28:10 +0200

    all works fine from the master branch

---
+++ Chasok4/.ptmp544268/GITChasok-ForAllBrowsers-/Chasok4/DAL/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Chasok4.Models.Entities;
+using Chasok4.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Chasok4.DAL
+{  
+        public class ApplicationDbContext : IdentityDbContext<AppUser>
+        {
+
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<UserMessage> UserMessages { get; set; }
+        //public DbSet<ConversationRoom> Rooms { get; set; }
+
+        public ApplicationDbContext()
+                : base("ConnectionToChasok", throwIfV1Schema: false)
+            {
+            }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+        
+        
+
+        
+    }   
+}
--- Chasok4/Scripts/_references.js
+++ Chasok4/Scripts/_references.js
@@ -1,5 +1,4 @@
-﻿/// <reference path="jquery-2.2.4.js" />
-/// <autosync enabled="true" />
+﻿/// <autosync enabled="true" />
 /// <reference path="bootstrap.min.js" />
 /// <reference path="jquery.signalr-2.2.1.min.js" />
 /// <reference path="jquery.validate.min.js" />
